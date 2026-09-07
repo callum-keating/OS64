915fc6eaad5491655537f0140fcca00f9d4a2bfa
callum <callumK@tutamail.com>
2026-09-07 18:00:40 +1000

    switched to a higher half kennel

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -58,7 +58,7 @@ size_t strlen(const char* str)
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
-uint16_t* terminal_buffer = (uint16_t*)VGA_MEMORY;
+uint16_t* terminal_buffer = (uint16_t*) 0xC03FF000;
 
 void terminal_initialize(void) 
 {
